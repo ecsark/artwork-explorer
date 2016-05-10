fca82e1e5bc92de964af2aa21b4fbb80c08c2e91
Edward Cheng <ecsark@gmail.com>
2016-05-10 13:54:14 -0400

    both upload and url works

--- frontend/mona_lisa.py
+++ frontend/mona_lisa.py
@@ -2,199 +2,182 @@ import numpy as np
 import caffe
 import cv2
 from scipy.spatial import distance
-from sklearn import svm
-import cPickle as pickle
-import os, sys
+import cPickle as pk
 import subprocess
 import math
 import lmdb
 from skimage.feature import local_binary_pattern
 from scipy.stats import itemfreq
 
+
 class VGGExtractorCPP:
-    def __init__(self, input_file, dst_image, output_dir, 
-                model_def, model_weights, extract_feature_bin, feature_def):
-        self.run_command = [extract_feature_bin, model_weights, model_def, feature_def, output_dir, '1', 'lmdb']
-        self.output_dir = output_dir
-        self.input_file = input_file
-        self.dst_image = dst_image
+	def __init__(self, input_file, dst_image, output_dir,
+	             model_def, model_weights, extract_feature_bin, feature_def):
+		self.run_command = [extract_feature_bin, model_weights, model_def, feature_def, output_dir, '1', 'lmdb']
+		self.output_dir = output_dir
+		self.input_file = input_file
+		self.dst_image = dst_image
 
-    @staticmethod
-    def read_from_db(db_dir):
-        lmdb_env = lmdb.open(db_dir)
-        lmdb_txn = lmdb_env.begin()
-        lmdb_cursor = lmdb_txn.cursor()
-        datum = caffe.proto.caffe_pb2.Datum()
-        D = []
-        for idx, (key, value) in enumerate(lmdb_cursor):
-            datum.ParseFromString(value)
-            data = caffe.io.datum_to_array(datum)
-            D.append(data.flatten())
+	@staticmethod
+	def read_from_db(db_dir):
+		lmdb_env = lmdb.open(db_dir)
+		lmdb_txn = lmdb_env.begin()
+		lmdb_cursor = lmdb_txn.cursor()
+		datum = caffe.proto.caffe_pb2.Datum()
+		D = []
+		for idx, (key, value) in enumerate(lmdb_cursor):
+			datum.ParseFromString(value)
+			data = caffe.io.datum_to_array(datum)
+			D.append(data.flatten())
 
-        lmdb_env.close()
-        return D
+		lmdb_env.close()
+		return D
 
-    def extract(self, image):
-        with open(self.dst_image, 'wb') as f:
-            f.write(image)
-        subprocess.call(['rm', '-rf', self.output_dir])
-        ret = subprocess.call(self.run_command)
-        if ret == 0:
-            return self.read_from_db(self.output_dir)[0]
-        else:
-            print "Feature extraction failed"
+	def extract(self, image):
+		with open(self.dst_image, 'wb') as f:
+			f.write(image)
+		subprocess.call(['rm', '-rf', self.output_dir])
+		ret = subprocess.call(self.run_command)
+		if ret == 0:
+			return self.read_from_db(self.output_dir)[0]
+		else:
+			print "Feature extraction failed"
+
 
 class VGGExtractor:
-    def __init__(self, model_weights, model_def,
-                mean=[104.0, 117.0, 123.0], 
-                new_shape=(256, 256), crop=(244, 244)):
-        self.net = caffe.Net(model_def, model_weights, caffe.TEST)
-        self.net.blobs['data'].reshape(1, 3, 224, 224)
-        self.transformer = caffe.io.Transformer({'data': self.net.blobs['data'].data.shape})
-        self.transformer.set_transpose('data', (2,0,1))
-        self.transformer.set_mean('data', np.array(mean))
-        self.transformer.set_raw_scale('data', 255)
-        self.transformer.set_channel_swap('data', (2,1,0))
-        self.new_shape = new_shape
-        self.crop = crop
+	def __init__(self, model_weights, model_def,
+	             mean=[104.0, 117.0, 123.0],
+	             new_shape=(256, 256), crop=(244, 244)):
+		self.net = caffe.Net(model_def, model_weights, caffe.TEST)
+		self.net.blobs['data'].reshape(1, 3, 224, 224)
+		self.transformer = caffe.io.Transformer({'data': self.net.blobs['data'].data.shape})
+		self.transformer.set_transpose('data', (2, 0, 1))
+		self.transformer.set_mean('data', np.array(mean))
+		self.transformer.set_raw_scale('data', 255)
+		self.transformer.set_channel_swap('data', (2, 1, 0))
+		self.new_shape = new_shape
+		self.crop = crop
 
-    def reshape_image(self, image, new_dim=None, crop_dim=None):
-        if new_dim is None:
-            new_dim = self.new_shape
-        if crop_dim is None:
-            crop_dim = self.crop
-        reshaped_img = cv2.resize(image, new_dim)
-        crop_amt = (new_dim[0]-crop_dim[0])/2, (new_dim[1]-crop_dim[1])/2
-        return reshaped_img[crop_amt[0]:crop_amt[0]+crop_dim[0], 
-                            crop_amt[1]:crop_amt[1]+crop_dim[1]]
+	def reshape_image(self, image, new_dim=None, crop_dim=None):
+		if new_dim is None:
+			new_dim = self.new_shape
+		if crop_dim is None:
+			crop_dim = self.crop
+		reshaped_img = cv2.resize(image, new_dim)
+		crop_amt = (new_dim[0] - crop_dim[0]) / 2, (new_dim[1] - crop_dim[1]) / 2
+		return reshaped_img[crop_amt[0]:crop_amt[0] + crop_dim[0],
+		       crop_amt[1]:crop_amt[1] + crop_dim[1]]
 
-    def extract(self, image, features=['fc7'], new_dim=None, crop_dim=None):
-        if new_dim is None:
-            new_dim = self.new_shape
-        if crop_dim is None:
-            crop_dim = self.crop
-        self.net.blobs['data'].data[...] = self.transformer.preprocess('data', 
-                                            self.reshape_image(image, new_dim, crop_dim))
-        _ = self.net.forward()
-        return [self.net.blobs[ft].data[0].tolist() for ft in features]
+	def extract(self, image, features=['fc7'], new_dim=None, crop_dim=None):
+		if new_dim is None:
+			new_dim = self.new_shape
+		if crop_dim is None:
+			crop_dim = self.crop
+		self.net.blobs['data'].data[...] = self.transformer.preprocess('data',
+		                                                               self.reshape_image(image, new_dim, crop_dim))
+		_ = self.net.forward()
+		return [self.net.blobs[ft].data[0].tolist() for ft in features]
 
 
 class LBPExtractor:
-    def __init__(self, radius=3, no_points=None):
-        self.radius = radius
-        if no_points is None:
-            self.no_points = 8 * self.radius
-        else:
-            self.no_points = no_points
+	def __init__(self, radius=3, no_points=None):
+		self.radius = radius
+		if no_points is None:
+			self.no_points = 8 * self.radius
+		else:
+			self.no_points = no_points
 
-    def extract(self, img):
-        im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        lbp = local_binary_pattern(im_gray, self.no_points, self.radius, method='uniform')
-        x = itemfreq(lbp.ravel())
-        return x[:, 1]/sum(x[:, 1])
+	def extract(self, img):
+		im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		lbp = local_binary_pattern(im_gray, self.no_points, self.radius, method='uniform')
+		x = itemfreq(lbp.ravel())
+		return x[:, 1] / sum(x[:, 1])
 
 
 class HogExtractor:
-    def __init__(self, n_bin=16):
-        self.n_bin = n_bin
+	def __init__(self, n_bin=16):
+		self.n_bin = n_bin
 
-    def extract(self, img):
-        gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
-        gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
-        mag, ang = cv2.cartToPolar(gx, gy)
-        bins = np.int32(self.n_bin*ang/(2*np.pi))
-        bin_cells = bins[:10,:10], bins[10:,:10], bins[:10,10:], bins[10:,10:]
-        mag_cells = mag[:10,:10], mag[10:,:10], mag[:10,10:], mag[10:,10:]
-        hists = [np.bincount(b.ravel(), m.ravel(), self.n_bin) for b, m in zip(bin_cells, mag_cells)]
-        hist = np.hstack(hists)
-        return hist
+	def extract(self, img):
+		gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+		gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+		mag, ang = cv2.cartToPolar(gx, gy)
+		bins = np.int32(self.n_bin * ang / (2 * np.pi))
+		bin_cells = bins[:10, :10], bins[10:, :10], bins[:10, 10:], bins[10:, 10:]
+		mag_cells = mag[:10, :10], mag[10:, :10], mag[:10, 10:], mag[10:, 10:]
+		hists = [np.bincount(b.ravel(), m.ravel(), self.n_bin) for b, m in zip(bin_cells, mag_cells)]
+		hist = np.hstack(hists)
+		return hist
 
 
 class PretrainedSVC:
-    def __init__(self, model):
-        self.svc = pickle.load(open(model, 'rb'))
+	def __init__(self, model):
+		self.svc = pk.load(open(model, 'rb'))
 
-    def predict(self, image_features):
-        return self.svc.predict(image_features)
+	def predict(self, image_features):
+		return self.svc.predict(image_features)
 
-    def get_decision(self, fts):
-        decisions = self.svc.decision_function(fts)
-        n_classes = int((1+math.sqrt(1+8*len(decisions[0]))) / 2)
-        scores = []
-        idx = 0
-        for i in range(n_classes):
-            i_score = []
-            for j in range(i):
-                i_score.append(-scores[j][i])
-            i_score.append(0)
-            for k in range(n_classes - i - 1):
-                i_score.append(decisions[0][idx])
-                idx += 1
-            scores.append(i_score)
-        return scores
+	def get_decision(self, fts):
+		decisions = self.svc.decision_function(fts)
+		n_classes = int((1 + math.sqrt(1 + 8 * len(decisions[0]))) / 2)
+		scores = []
+		idx = 0
+		for i in range(n_classes):
+			i_score = []
+			for j in range(i):
+				i_score.append(-scores[j][i])
+			i_score.append(0)
+			for k in range(n_classes - i - 1):
+				i_score.append(decisions[0][idx])
+				idx += 1
+			scores.append(i_score)
+		return scores
 
-    @staticmethod
-    def convert_to_votes(decision):
-        return (np.array(decision)>0).sum(axis=1).tolist()
+	@staticmethod
+	def convert_to_votes(decision):
+		return (np.array(decision) > 0).sum(axis=1).tolist()
 
-    def get_votes(self, fts):
-        return self.convert_to_votes(self.get_decision(fts))
+	def get_votes(self, fts):
+		return self.convert_to_votes(self.get_decision(fts))
 
-    def get_top_k_classes(self, votes, k=10):
-        return [(self.svc.classes_[i], votes[i]) for i in np.argsort(votes)[::-1][:k]]
+	def get_top_k_classes(self, votes, k=10):
+		return [(self.svc.classes_[i], votes[i]) for i in np.argsort(votes)[::-1][:k]]
+
 
 class Recommender(object):
-    def __init__(self, pool):
-        self.pool = pool
-    
-    @staticmethod
-    def findMostSimilar(X, x_query, k=6, f='euclidean'):
-        dist = distance.cdist(X, np.array([x_query]), f)
-        return dist.flatten().argsort()[:k]
+	def __init__(self, pool):
+		self.pool = pool
 
-    def recommend(self, input_ft, k=6, from_list=None, f='euclidean'):
-        if from_list is None:
-            pool = self.pool
-            from_list = range(len(self.pool))
-        else:
-            pool = [self.pool[i] for i in from_list]
-        k_sim_idx = self.findMostSimilar(pool, input_ft, k, f)
-        return [from_list[i] for i in k_sim_idx]
+	@staticmethod
+	def findMostSimilar(X, x_query, k=6, f='euclidean'):
+		dist = distance.cdist(X, np.array([x_query]), f)
+		return dist.flatten().argsort()[:k]
+
+	def recommend(self, input_ft, k=6, from_list=None, f='euclidean'):
+		if from_list is None:
+			pool = self.pool
+			from_list = range(len(self.pool))
+		else:
+			pool = [self.pool[i] for i in from_list]
+		k_sim_idx = self.findMostSimilar(pool, input_ft, k, f)
+		return [from_list[i] for i in k_sim_idx]
 
 
 class ZeroScoreRecommender(Recommender):
-    def __init__(self, all_votes):
-        super(ZeroScoreRecommender, self).__init__(all_votes)
+	def __init__(self, all_votes):
+		super(ZeroScoreRecommender, self).__init__(all_votes)
 
-    @staticmethod
-    def convert_to_votes(decision):
-        return (np.array(decision)>0).sum(axis=1).tolist()
+	@staticmethod
+	def convert_to_votes(decision):
+		return (np.array(decision) > 0).sum(axis=1).tolist()
 
-    def recommend(self, votes, k=6, from_list=None, f='cityblock'):
-        return super(ZeroScoreRecommender, self).recommend(votes, k, from_list, f)
-
-
-class MonaLisa:
-    def __init__(self, models={}):
-        self.models = models
-
-    def add_model(self, name, feature_engine, pred_model):
-        if name in self.models:
-            print 'WARNING: model ' + name + ' already exists in MonaLisa'
-        self.models[name] = (feature_engine, pred_model)
-
-    def predict(self, image):
-        ans = []
-        for k, v in self.models.items():
-            ft = v[0].extract(image)[0]
-            ans.append(v[1].predict(ft)[0])
-        return ans
-
-
+	def recommend(self, votes, k=6, from_list=None, f='cityblock'):
+		return super(ZeroScoreRecommender, self).recommend(votes, k, from_list, f)
 
 
 def main():
-    pass
+	pass
+
 
 """
 import caffe    
--- frontend/server.py
+++ frontend/server.py
@@ -1,27 +1,27 @@
-#!/usr/bin/env python2.7
-
 import os
 import errno
 from flask import Flask, g, request, render_template, redirect
 from sqlalchemy import *
 import random
 from werkzeug import secure_filename
-import caffe		
+import caffe
 from mona_lisa import VGGExtractor, HogExtractor, LBPExtractor, PretrainedSVC, ZeroScoreRecommender, Recommender
 from flask_bootstrap import Bootstrap
 import cPickle as pk
 import cv2
 import scipy.stats
-
+import urllib
+import numpy as np
+import skimage
 
 UPLOAD_FOLDER = '/Users/ecsark/Documents/visualdb/project/artwork-explorer/frontend/static/upload/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 img_dir = "/Users/ecsark/Documents/visualdb/project/wikiart/images/"
 
-app = Flask(__name__, template_folder=tmpl_dir)
+app = Flask(__name__, template_folder=template_dir)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = os.urandom(24)
 Bootstrap(app)
@@ -42,23 +42,18 @@ lbp_recommender = Recommender(pk.load(open(proj_root + 'model/lbp_train.pk', 'rb
 artist_recommender = ZeroScoreRecommender(pk.load(open(proj_root + 'model/artist_decision_votes_train.pk', 'rb')))
 year_recommender = ZeroScoreRecommender(pk.load(open(proj_root + 'model/year_decision_scores_train.pk', 'rb')))
 
+engine = create_engine("postgresql://localhost/artdb")
 
-DATABASEURI = "postgresql://localhost/artdb"
-engine = create_engine(DATABASEURI)
+style_name = ["Art Nouveau", "Baroque",
+              "Expressionism", "Impressionism",
+              "Neoclassicism", "Post-Impressionism",
+              "Realism", "Romanticism",
+              "Surrealism", "Symbolism"]
 
-style_name = ["Art Nouveau", "Baroque", 
-							"Expressionism", "Impressionism", 
-							"Neoclassicism", "Post-Impressionism", 
-							"Realism", "Romanticism", 
-							"Surrealism", "Symbolism"]
 
-class img_info:
-	def __init__(self, img_id, style, artist, name, url):
-		self.img_id = img_id
-		self.style = get_style_name(style)
-		self.artist = artist
-		self.name = name
-		self.url = url
+def get_style_name(style_id):
+	return style_name[style_id]
+
 
 @app.before_request
 def before_request():
@@ -73,8 +68,10 @@ def before_request():
 		g.conn = engine.connect()
 	except:
 		print "uh oh, problem connecting to database"
-		import traceback; traceback.print_exc()
+		import traceback
+		traceback.print_exc()
 		g.conn = None
+
 
 @app.teardown_request
 def teardown_request(exception):
@@ -85,36 +82,49 @@ def teardown_request(exception):
 	try:
 		g.conn.close()
 	except Exception as e:
-		pass
+		print e
+
 
 @app.route('/')
 def index():
 	return render_template("index.html")
 
+
 def allowed_file(filename):
-		return '.' in filename and \
-					 filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/upload_file', methods=['GET', 'POST'])
-def upload_file():
-		if request.method == 'POST':
-				file = request.files['file']
-				if file and allowed_file(file.filename):
-						filename = secure_filename(file.filename)
-						absolute_file_url = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-						file.save(absolute_file_url)
-						return analyze_and_render(absolute_file_url, '/static/upload/'+filename)
 
-def get_style_name(style_id):
-	return style_name[style_id]
+class ImageContainer:
+	def __init__(self, img_id, style, artist, year, name, url):
+		self.img_id = img_id
+		self.style = get_style_name(style)
+		self.artist = artist
+		self.name = name
+		self.year = int(year) if year is not None else None
+		self.url = url
+
+
+def url_to_image(url):
+	image = skimage.io.read(url)
+	return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	resp = urllib.urlopen(url)
+	image = np.asarray(bytearray(resp.read()), dtype=np.float32)
+	image = cv2.imdecode(image, -1)
+	return image
+
 
 def extract_feature(image_file_name):
-	image = caffe.io.load_image(image_file_name)
-	ft_vgg = vgg_ft.extract(image)
-	im = cv2.imread(image_file_name)
+	im = caffe.io.load_image(image_file_name)
+	ft_vgg = vgg_ft.extract(im)
+	im = im[:,:,(2,1,0)]*255
+	# try:
+	# 	im = cv2.imread(image_file_name)
+	# except:
+	# 	im = url_to_image(image_file_name)
 	ft_hog = hog_ft.extract(im)
 	ft_lbp = lbp_ft.extract(im)
-	return {'vgg':ft_vgg, 'hog':ft_hog, 'lbp':ft_lbp}
+	return {'vgg': ft_vgg, 'hog': ft_hog, 'lbp': ft_lbp}
+
 
 def analyze_style(features):
 	pred = style_svc.predict(features['vgg'])[0]
@@ -124,6 +134,7 @@ def analyze_style(features):
 	rec = hog_recommender.recommend(features['hog'], k=8, from_list=rec_2)
 	return pred, rec, votes
 
+
 def analyze_artist(features):
 	pred = artist_svc.predict(features['vgg'])[0]
 	votes = artist_svc.get_votes(features['vgg'])
@@ -132,130 +143,132 @@ def analyze_artist(features):
 	rec = hog_recommender.recommend(features['hog'], k=8, from_list=rec_2)
 	return pred, rec, votes
 
+
 def analyze_year(features):
-    pred = year_svr.predict(features['vgg'])[0]
-    votes = [pred]
-    rec_1 = year_recommender.recommend(votes, k=32)
-    rec_2 = lbp_recommender.recommend(features['lbp'], k=16, from_list=rec_1)
-    rec = hog_recommender.recommend(features['hog'], k=8, from_list=rec_2)
-    print rec
-    return pred, rec, votes 
+	pred = year_svr.predict(features['vgg'])[0]
+	votes = [pred]
+	rec_1 = year_recommender.recommend(votes, k=32)
+	rec_2 = lbp_recommender.recommend(features['lbp'], k=16, from_list=rec_1)
+	rec = hog_recommender.recommend(features['hog'], k=8, from_list=rec_2)
+	return pred, rec, votes
 
-
-def analyze_image(image_file_name):
-	image = caffe.io.load_image(image_file_name)
-	ft_vgg = vgg_ft.extract(image)
-
-	pred_style = style_svc.predict(ft_vgg)[0]
-	votes_style = style_svc.get_votes(ft_vgg)
-	im = cv2.imread(image_file_name)
-	ft_hog = hog_ft.extract(im)
-	ft_lbp = lbp_ft.extract(im)
-	rec_style_1 = style_recommender.recommend(votes_style, k=32)
-	rec_style_2 = lbp_recommender.recommend(ft_lbp, k=16, from_list=rec_style_1)
-	rec_style = hog_recommender.recommend(ft_hog, k=8, from_list=rec_style_2)
-
-	pred_artist = style_svc.predict(ft_vgg)[0]
-	votes_artist = artist_svc.get_votes(ft_vgg)
-	rec_artist_1 = artist_recommender.recommend(votes_artist, k=32)
-	rec_artist_2 = lbp_recommender.recommend(ft_lbp, k=16, from_list=rec_artist_1)
-	rec_artist = hog_recommender.recommend(ft_hog, k=8, from_list=rec_artist_2)
-	#recommendation = style_recommender.recommend(score, k=8)
-	return pred_style, rec_style, votes_style
-
-
-def analyze_and_render(absolute_file_url, get_url, id=0, artist='', name=''):
-	prediction, recommendation, poss = analyze_image(absolute_file_url)
-	results_list = []
-	for i in recommendation:
-		img = build_img_info(str(i), "imgs")
-		results_list.append(img)
-	source = img_info(id, prediction, artist, name, get_url)
-	return render_template("show_image.html", source = source, results_list= results_list, poss = poss)
 
 def get_result_id(src):
-	print src
 	return random.sample(range(0, 8000), 8)
 
-def build_img_info(img_id, table):
-	if img_id != None:
-		if table == "tests":
+
+def extract_artist_info(file_name):
+	a = file_name.split('.')[0].split('/')[-1].split('_')
+	artist = a[0].replace("-", " ").title()
+	b = a[-1].split('-')
+	year = None
+	name = None
+	if len(b) > 1 and b[-1].isdigit():
+		for idx, n in enumerate(reversed(b)):
+			if n.isdigit():
+				if len(n) == 4:
+					year = int(n)
+					name = ' '.join(b[:len(b)-idx])
+					break
+			else:
+				break
+	name = ' '.join(b).title() if name is None else name
+	return artist, name, year
+
+
+def build_img_info(img_id, table_name):
+	if img_id is not None:
+		if table_name == "tests":
 			cursor = g.conn.execute("SELECT * FROM tests WHERE id = %s", img_id)
-		elif table == "imgs":
+		elif table_name == "imgs":
 			cursor = g.conn.execute("SELECT * FROM imgs WHERE id = %s", img_id)
 		else:
-			return None	 
-		entry = cursor.fetchone() 
-		if entry != None:
+			return None
+		entry = cursor.fetchone()
+		if entry is not None:
 			url = "/static/img/" + entry['name']
-			info = entry['name'].split("_")
-			artist = info[0].replace("-"," ").title()
-			name = (info[1].replace("-", " ")).replace(".jpg","").title()
-			return img_info(img_id, entry['style'], artist, name, url)
-	cursor.close()
-	return None	 
-
-@app.route('/query_url')
-def query_url():
-	img_url = request.args.get("img_url")
-	if img_url != None:
-		try:
-			return analyze_and_render(img_url,	img_url)
-		except:
-			pass
-	return "invalid image url" 
-
-
-def build_result(features, analyze_fn):
-	pred, rec, votes = analyze_fn(features)
-	results = list()
-	for i in rec:
-				img = build_img_info(str(i), "imgs")
-				results.append(img)
-	return results, votes, pred
+			artist, name, year = extract_artist_info(entry['name'])
+			return ImageContainer(img_id, entry['style'], artist, year, name, url)
+		cursor.close()
 
 
 def parse_name(st):
 	return ' '.join(st.split('-')).title()
 
 
+def build_result(features, analyze_fn):
+	pred, rec, votes = analyze_fn(features)
+	results = list()
+	for i in rec:
+		img = build_img_info(str(i), "imgs")
+		results.append(img)
+	return results, votes, pred
+
+
+def analyze_and_render(absolute_file_url, get_url, source=None):
+	features = extract_feature(absolute_file_url)
+	style_results, style_votes, style_pred = build_result(features, analyze_style)
+	artist_results, artist_votes, artist_pred = build_result(features, analyze_artist)
+	year_results, year_votes, year_pred = build_result(features, analyze_year)
+	style_show = sorted([(style_name[i], v) for i, v in enumerate(style_votes)], key=lambda x: x[1])
+	artist_show = artist_svc.get_top_k_classes(artist_votes, 10)
+	artist_min = min([a[1] for a in artist_show]) - 10
+	artist_show = sorted([(parse_name(a[0]), a[1] - artist_min) for a in artist_show], key=lambda x: x[1])
+
+	norm = scipy.stats.norm(year_pred, 40)
+	year_start = int(year_pred / 10) * 10 - 40
+	year_show = [(str(y), norm.pdf(y) * 10) for y in range(year_start, year_start + 100, 10)]
+
+	internal_source = source is not None
+	if source is None:
+		source = ImageContainer(0, style_pred, artist_pred.title(), year_pred, None, get_url)
+
+	return render_template("show_image.html", source=source, internal_source=internal_source,
+	                       styles=style_show, style_pics=style_results,
+	                       artists=artist_show, artist_pics=artist_results,
+	                       years=year_show, year_pics=year_results)
+
+
+@app.route('/query_url')
+def query_url():
+	img_url = request.args.get("img_url")
+	if img_url is None:
+		return "invalid image url"
+	try:
+		return analyze_and_render(img_url, img_url)
+	except Exception as e:
+	 	print e
+
+
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['file']
+		if f and allowed_file(f.filename):
+			filename = secure_filename(f.filename)
+			absolute_file_url = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+			f.save(absolute_file_url)
+			return analyze_and_render(absolute_file_url, '/static/upload/' + filename)
+
+
 @app.route('/query')
 def query():
 	img_id = request.args.get("img_id")
-	if img_id != None:
+	if img_id is not None:
 		source = build_img_info(img_id, "tests")
-		if source != None:
-			filename = source.url[12:]
-			absolute_file_url = img_dir+filename
-			features = extract_feature(absolute_file_url)
-			style_results = build_result(features, analyze_style)
-			artist_results = build_result(features, analyze_artist)
-			year_results = build_result(features, analyze_year)
-			style_show = sorted([(style_name[i], v) for i, v in enumerate(style_results[1])], key=lambda x: x[1])
-			artist_show = artist_svc.get_top_k_classes(artist_results[1], 10)
-			artist_min = min([a[1] for a in artist_show])-10
-			artist_show = sorted([(parse_name(a[0]), a[1] - artist_min) for a in artist_show], key=lambda x: x[1])
-
-			year_pred = year_results[2]
-			norm = scipy.stats.norm(year_pred, 40)
-			year_start =  int(year_pred/10) * 10 - 40
-			year_show = [(str(y), norm.pdf(y)*10) for y in range(year_start, year_start+100, 10)]
-
-			source.url = '/static/img/'+filename
-			return render_template("show_image.html", source=source, 
-				styles=style_show, style_pics=style_results[0], 
-				artists=artist_show, artist_pics=artist_results[0],
-				years=year_show, year_pics=year_results[0])
-		else:
-			return "img does not exist" 
+		if source is None:
+			return "img does not exist"
+		filename = source.url[12:]
+		absolute_file_url = img_dir + filename
+		return analyze_and_render(absolute_file_url, '/static/img/' + filename, source)
 
 
 @app.route('/user_grouping', methods=['GET', 'POST'])
 def user_grouping():
 	uid = request.args.get('uid')
-	if uid == None:
+	if uid is None:
 		uid = 'Please type your name'
-	img_id = random.randint(0,1999)
+	img_id = random.randint(0, 1999)
 	source = build_img_info(img_id, "tests")
 	results_id = get_result_id(int(img_id))
 	results_list = list()
@@ -263,7 +276,7 @@ def user_grouping():
 		img = build_img_info(str(i), "imgs")
 		results_list.append(img)
 
-	return render_template("user_grouping.html", source = source, results_list = results_list, uid = uid, all_id = results_id)
+	return render_template("user_grouping.html", source=source, results_list=results_list, uid=uid, all_id=results_id)
 
 
 @app.route('/group_result', methods=['GET', 'POST'])
@@ -273,19 +286,21 @@ def group_result():
 	img_id = request.form.get('img_id')
 	uid = request.form.get('uid')
 	if img_id != None:
-		g.conn.execute("INSERT INTO user_group (id, source_id, pics, uid, all_id) VALUES (DEFAULT, %s, %s, %s, %s);", img_id, selected, uid, all_id)
+		g.conn.execute("INSERT INTO user_group (id, source_id, pics, uid, all_id) VALUES (DEFAULT, %s, %s, %s, %s);",
+		               img_id, selected, uid, all_id)
 
 	if uid != None:
-		return redirect('/user_grouping?uid='+uid)
+		return redirect('/user_grouping?uid=' + uid)
 	else:
-		return redirect('/user_grouping')	
+		return redirect('/user_grouping')
 
 
 @app.route('/user_test')
 def get_test_img():
-	img_id = random.randint(0,1999)
+	img_id = random.randint(0, 1999)
 	source = build_img_info(img_id, "tests")
-	return render_template("user_test.html", source = source, img_id = img_id)
+	return render_template("user_test.html", source=source, img_id=img_id)
+
 
 @app.route('/new_pair')
 def new_pair():
@@ -293,12 +308,12 @@ def new_pair():
 	img_id = request.args.get('img_id')
 	cursor = g.conn.execute("INSERT INTO user_conv (id, src, style) VALUES (DEFAULT, %s, %s);", img_id, select)
 	return redirect('user_test')
-	img_id = random.randint(0,1999)
-	source = build_img_info(img_id, "tests")
-	return render_template("user_test.html", source = source, img_id = img_id)
+
 
 if __name__ == "__main__":
 	import click
+
+
 	@click.command()
 	@click.option('--debug', is_flag=True)
 	@click.option('--threaded', is_flag=True)
@@ -323,9 +338,9 @@ if __name__ == "__main__":
 				os.remove(static_dir + "/img")
 				os.symlink(img_dir, static_dir + "/img")
 
-
 		HOST, PORT = host, port
 		print "running on %s:%d" % (HOST, PORT)
 		app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
-		
+
+
 	run()
